9c65cbf311d4f7ddeb6edf309ac4f3216d5dc3bc
Christopher Santana <christopher.santana@nav.no>
2023-01-06 14:54:00 +0100

    DEBUG.

--- script/distribute_default_dependencies.sh
+++ script/distribute_default_dependencies.sh
@@ -8,12 +8,12 @@ fi
 ## Sort and filter duplicates
 REPOSITORIES=$(echo -e "$REPOSITORIES" | sort | uniq)
 
-iteration = 1
+iteration=1
 
 ## Distribute files for each project
 for repository in $REPOSITORIES; do
-  echo iteration $iteration
-  (( $iteration += 1 ))
+  echo "iteration $iteration"
+  ((iteration += 1))
   if [[ $repository == $GITHUB_REPOSITORY ]]; then
     echo "Should not distribute files to same repository. Skipping $repository"
   else
