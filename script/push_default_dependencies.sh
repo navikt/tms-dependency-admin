716f4c203fc6d0e3f1ffb565ff2369c44ad84bb5
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2023-07-31 11:44:48 +0200

    legg til venteperiode før commitoppdateirng

--- script/push_default_dependencies.sh
+++ script/push_default_dependencies.sh
@@ -128,6 +128,8 @@ CREATE_BRANCH_PAYLOAD=$(jq -n -c \
                       '{ ref: $ref, sha: $sha }'
 )
 
+sleep 2
+
 curl -s -X POST -u "$API_ACCESS_TOKEN:" --data "$CREATE_BRANCH_PAYLOAD" "https://api.github.com/repos/$REPOSITORY/git/refs" > /dev/null
 
 ## Push new commit
