a09c3e6eeba2f030ee873548614e651b5ac691e1
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2023-07-31 11:24:10 +0200

    gi ut mer info på curl

--- script/push_default_dependencies.sh
+++ script/push_default_dependencies.sh
@@ -121,7 +121,7 @@ CREATE_BRANCH_PAYLOAD=$(jq -n -c \
                       '{ ref: $ref, sha: $sha }'
 )
 
-curl -s -X POST -u "$API_ACCESS_TOKEN:" --data "$CREATE_BRANCH_PAYLOAD" "https://api.github.com/repos/$REPOSITORY/git/refs" > /dev/null
+curl -i -X POST -u "$API_ACCESS_TOKEN:" --data "$CREATE_BRANCH_PAYLOAD" "https://api.github.com/repos/$REPOSITORY/git/refs" > /dev/null
 
 ## Push new commit
 PUSH_COMMIT_PAYLOAD=$(jq -n -c \
@@ -129,6 +129,6 @@ PUSH_COMMIT_PAYLOAD=$(jq -n -c \
                       '{ sha: $sha, force: false }'
 )
 
-BRANCH_SHA=$(curl -s -X PATCH -u "$API_ACCESS_TOKEN:" --data "$PUSH_COMMIT_PAYLOAD" "https://api.github.com/repos/$REPOSITORY/git/refs/heads/$BRANCH_NAME" | jq -r '.object.sha')
+BRANCH_SHA=$(curl -i -X PATCH -u "$API_ACCESS_TOKEN:" --data "$PUSH_COMMIT_PAYLOAD" "https://api.github.com/repos/$REPOSITORY/git/refs/heads/$BRANCH_NAME" | jq -r '.object.sha')
 
 echo "Branch $BRANCH_NAME is now on commit $BRANCH_SHA"
