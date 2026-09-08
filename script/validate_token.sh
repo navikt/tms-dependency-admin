40dd27b65c64058b07371d2b5d8a0020717cea83
Christopher Santana <christopher.santana@nav.no>
2026-09-08 13:50:10 +0200

    Endrer curl-kall.

--- script/validate_token.sh
+++ script/validate_token.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Make headers-only request to github and extract scopes-header
-SCOPE_HEADER=$(curl -X HEAD https://api.github.com -H "Authorization: token $API_ACCESS_TOKEN" -I -s | grep 'x-oauth-scopes')
+SCOPE_HEADER=$(curl -I https://api.github.com -H "Authorization: token $API_ACCESS_TOKEN" -I -s | grep 'x-oauth-scopes')
 
 ## Extract scopes from header
 IFS=', ' read -r -a TOKEN_SCOPES <<< $(echo $SCOPE_HEADER | sed 's/x-oauth-scopes: \(.*\)\r/\1/')
